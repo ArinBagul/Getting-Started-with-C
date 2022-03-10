a108b2bf546bbb5c320f0591e2de78681f164167
ArinBagul <73395201+ArinBagul@users.noreply.github.com>
2022-03-10 11:16:11 +0530

    Add files via upload

---
+++ tut-03.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    //basic date type : Size
+    /*printf("The Size of Int is: %d\n", sizeof(int));
+    printf("The Size of float is: %d\n", sizeof(float));
+    printf("The Size of char is: %d\n", sizeof(char));*/
+
+    // datatype var_name;
+    int a;
+    int b;
+    double cat;
+    a = 10;
+    printf("The value of a is %d", a);
+    
+    return 0;
+}
